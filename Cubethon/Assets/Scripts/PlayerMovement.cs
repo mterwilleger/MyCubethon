227a58cc370e35c01eaf3bf137902ccaea3e5f3e
mterwilleger <93240105+mterwilleger@users.noreply.github.com>
2023-03-06 17:08:47 -0700

    Cubethon Initialization

---
+++ Cubethon/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //This is a reference to the Rigidbody component called "rb"
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    // We marked this as "FixedUpdate" becasue we are using it to mess with physics
+
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime); //Add a forward force on the z-axis
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
