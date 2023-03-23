c5179aecb9e08941a57967e9d8b45534de9a0255
mterwilleger <93240105+mterwilleger@users.noreply.github.com>
2023-03-22 20:42:16 -0700

    Added Flying/Jump abilities

--- Cubethon/Assets/Scripts/PlayerMovement.cs
+++ Cubethon/Assets/Scripts/PlayerMovement.cs
@@ -4,14 +4,37 @@ public class PlayerMovement : MonoBehaviour
 {
     //This is a reference to the Rigidbody component called "rb"
     public Rigidbody rb;
+    private BoxCollider _col;
 
     public float forwardForce = 2000f;
     public float sidewaysForce = 500f;
+    public float jumpVelocity = 1f;
+    public float distanceToGround = 0.1f;
+    public LayerMask groundLayer;
+    private bool doJump = false;
+
+    void Start()
+    {
+        _col = GetComponent<BoxCollider>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKey("w"))
+        {
+            doJump = true;
+        }
+    }
 
     // We marked this as "FixedUpdate" becasue we are using it to mess with physics
 
     void FixedUpdate()
     {
+        if (doJump)
+        {
+            rb.AddForce(Vector3.up / jumpVelocity, ForceMode.Impulse);
+            doJump = false;
+        }
         rb.AddForce(0, 0, forwardForce * Time.deltaTime); //Add a forward force on the z-axis
 
         if (Input.GetKey("d"))
